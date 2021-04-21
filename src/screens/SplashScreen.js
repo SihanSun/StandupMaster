f8e3dd3e1f77a6979800c7f31f08e6dd474f7250
Kai Li <kal182@ucsd.edu>
2021-04-20 20:19:56 -0700

    avoid repeating useEffect

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -47,7 +47,7 @@ export default function SplashScreen({ navigation }) {
 
     useEffect(() => {
         checkUser();
-    });
+    }, []);
 
     return (
         <SafeAreaView style={styles.safeAreaContainer}>
